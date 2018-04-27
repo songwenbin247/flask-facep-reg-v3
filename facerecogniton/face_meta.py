4f6de07579ab811b48e110a536ced3f7d54ce0d0
Xiaomin Lu <xiaomin.lu@nxp.com>
2018-04-27 02:00:24 +0000

    add face_meta.py

---
+++ facerecogniton/face_meta.py
@@ -0,0 +1,9 @@
+class FaceMeta:
+    def __init__(self):
+        self.detected = 0
+        self.mean = 0
+        self.var = 0
+        self.cur_state = 0
+        self.cur_time = 0
+        self.cur_location = []
+
